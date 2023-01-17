cd60282b1925bd6024c77566516ed9d75be9f34d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-17 18:03:46 +0530

    Db Context

---
+++ Source/Sports.Persistence/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sports.Data.Entities;
+
+namespace Sports.Persistence.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba411"), Name = "Active Wear - Men" },
+                new Category { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba412"), Name = "Active Wear - Women" },
+                new Category { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba413"), Name = "Mineral Water" },
+                new Category { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba414"), Name = "Publications" },
+                new Category { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba415"), Name = "Supplements" });
+
+        }
+
+    }
+}
