667cf57ec7c73ca4ffd701d7681bc4e1480c6d51
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-17 18:42:10 +0530

    Update ModelBuilderExtensions.cs

--- Source/Sports.Persistence/Extensions/ModelBuilderExtensions.cs
+++ Source/Sports.Persistence/Extensions/ModelBuilderExtensions.cs
@@ -15,6 +15,18 @@ namespace Sports.Persistence.Extensions
                 new Category { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba414"), Name = "Publications" },
                 new Category { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba415"), Name = "Supplements" });
 
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba501"), CategoryId = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba411"), Name = "Grunge Skater Jeans", Sku = "AWMGSJ", Price = 68, IsAvailable = true },
+                new Product { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba502"), CategoryId = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba411"), Name = "Polo Shirt", Sku = "AWMPS", Price = 35, IsAvailable = true },
+                new Product { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba503"), CategoryId = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba411"), Name = "Skater Graphic T-Shirt", Sku = "AWMSGT", Price = 33, IsAvailable = true },
+                new Product { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba504"), CategoryId = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba411"), Name = "Slicker Jacket", Sku = "AWMSJ", Price = 125, IsAvailable = true },
+                new Product { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba505"), CategoryId = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba411"), Name = "Thermal Fleece Jacket", Sku = "AWMTFJ", Price = 60, IsAvailable = true },
+                new Product { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba506"), CategoryId = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba411"), Name = "Unisex Thermal Vest", Sku = "AWMUTV", Price = 95, IsAvailable = true },
+                new Product { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba507"), CategoryId = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba411"), Name = "V-Neck Pullover", Sku = "AWMVNP", Price = 65, IsAvailable = true },
+                new Product { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba508"), CategoryId = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba411"), Name = "V-Neck Sweater", Sku = "AWMVNS", Price = 65, IsAvailable = true },
+                new Product { Id = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba509"), CategoryId = Guid.Parse("62042340-8b07-45c7-8e93-70ea318ba411"), Name = "V-Neck T-Shirt", Sku = "AWMVNT", Price = 17, IsAvailable = true }
+                );
+
         }
 
     }
