2a7b0b234379e76ca920e4b64ea2b2b7dbe06f62
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-19 13:54:05 +0530

    Update Product.cs

--- Source/Sports.Data/Entities/Product.cs
+++ Source/Sports.Data/Entities/Product.cs
@@ -6,10 +6,13 @@ namespace Sports.Data.Entities
 
     public class Product : BaseEntity
     {
+        [Required]
         public string Sku { get; set; } = string.Empty;
 
+        [Required]
         public string Name { get; set; } = string.Empty;
 
+        [Required]
         public string Description { get; set; } = string.Empty;
 
         public decimal Price { get; set; }
