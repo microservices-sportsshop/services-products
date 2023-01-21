a6d241b5201edacd2c9392eaa9ec9f2145f5d352
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-21 15:00:38 +0530

    Create ProductDto.cs

---
+++ Source/Sports.Data/Dtos/ProductDto.cs
@@ -0,0 +1,25 @@
+﻿using Sports.Data.Entities;
+
+namespace Sports.Data.Dtos
+{
+
+    public record ProductDto
+    {
+        public Guid Id { get; set; }
+
+        public string Sku { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+    }
+
+}
