f04a19ca8d950844aadd8e120b2f17569649395d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-25 07:03:19 +0530

    Swamy/24jan work (#7)
    * Extension Method for Configuring Http Request Pipeline
    
    * Update Program.cs
    
    * Update SportsShopDbContext.cs

---
+++ Source/Sports.API/Extensions/HttpRequestPipelineExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Sports.Persistence;
+
+namespace Sports.API.Extensions
+{
+
+    public static class HttpRequestPipelineExtensions
+    {
+
+        public static WebApplication ConfigureHttpRequestPipeline(this WebApplication app)
+        {
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+
+                using var scope = app.Services.CreateScope();
+                using var context = scope.ServiceProvider.GetService<SportsShopDbContext>();
+                _ = (context?.Database.EnsureCreated());
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+
+}
